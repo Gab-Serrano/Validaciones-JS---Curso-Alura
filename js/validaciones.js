f6d883b759463493dec3eda60189f3dbc075697e
Gabriel Serrano <gab.serrano@duocuc.cl>
2023-05-03 10:42:23 -0400

    update

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,31 +1,98 @@
 export function validate(input) {
     const inputType = input.dataset.type;
     if (validators[inputType]) {
-        console.log(validators[inputType]);
-        validators[inputType](input)
+        validators[inputType](input);
     }
 
+    if (input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    } else {
+        input.parentElement.classList.add("input-container--invalid");
+        /* showErrorMessage(inputType, input); */
+        input.parentElement.querySelector(".input-message-error").innerHTML = showErrorMessage(inputType, input);
+    };
 
 }
 
 const validators = {
     birth: input => validarNacimiento(input),
     name: input => validarNombre(input),
-
 };
 
-function validarNacimiento(input) {
-    const fechaNacimiento = new Date(input.value);
+function showErrorMessage(inputType, input) {
     let message = "";
-    if (!calcularMayoriaEdad(fechaNacimiento)) {
-        message = "Debes tener al menos 18 años de edad.";
+
+    const errorTypes = [
+        "valueMissing",
+        "typeMismatch",
+        "patternMismatch",
+        "customError"
+    ];
+
+    errorTypes.forEach((error) => {
+        if (input.validity[error]) {
+            message = errorMessages[inputType][error];
+        }
+    })
+
+    return message;
+    /* for (var key in input.validity) {
+        if (input.validity[key]) {
+            return errorMessages[inputType][key];
+        }
+    } */
+}
+
+const errorMessages = {
+    name: {
+        valueMissing: "Este campo no puede estar vacío.",
+        customError: "Debes ingresar tu nombre completo."
+    },
+    email: {
+        valueMissing: "Este campo no puede estar vacío.",
+        typeMismatch: "El correo no es válido."
+    },
+    password: {
+        valueMissing: "Este campo no puede estar vacío.",
+        patternMismatch: "La contraseña debe tener entre 6 y 12 caracteres. Debe contener minúsculas, mayúsculas y números."
+    },
+    birth: {
+        valueMissing: "Este campo no puede estar vacío.",
+        customError: "Debes tener al menos 18 años de edad.",
+    },
+    tel: {
+        valueMissing: "Este campo no puede estar vacío.",
+        patternMismatch: "El formato requerido es de 9XXXXXXXX"
+    },
+    adress: {
+        valueMissing: "Este campo no puede estar vacío.",
+        patternMismatch: "La dirección debe contener entre 5 a 40 caracteres."
+    },
+    city: {
+        valueMissing: "Este campo no puede estar vacío.",
+        patternMismatch: "El nombre de la ciudad debe contener entre 5 a 40 caracteres."
+    },
+    state: {
+        valueMissing: "Este campo no puede estar vacío.",
+        patternMismatch: "El nombre del estado debe contener entre 5 a 40 caracteres."
     }
-    input.setCustomValidity(message);
+}
+
+function validarNacimiento(input) {
+    if (!input.value == "") {
+        const fechaNacimiento = new Date(input.value);
+        let message = "";
+        if (!calcularMayoriaEdad(fechaNacimiento)) {
+            message = "Debes tener al menos 18 años de edad.";
+        }
+        input.setCustomValidity(message);
+    }
 }
 
 function validarNombre(input) {
     let message = "";
-    if (input.textContent.length < 5) {
+    if (input.value.length > 0 && input.value.length < 10) {
         message = "Debes ingresar tu nombre completo";
     }
     input.setCustomValidity(message);
